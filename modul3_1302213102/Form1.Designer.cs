e4ed934fe1cc72110262f05b743e614fef7269cf
PC10-IFLAB <PC10-IFLAB>
2023-03-08 16:50:37 +0700

    push
    menambahkan implementasi kalkulator

--- modul3_1302213102/Form1.Designer.cs
+++ modul3_1302213102/Form1.Designer.cs
@@ -179,8 +179,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(198, 20);
             this.textBox1.TabIndex = 13;
-            this.textBox1.Text = "Label Output";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
